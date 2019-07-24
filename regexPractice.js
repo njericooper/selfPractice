dc3c34ed4eafb8beb6a222d685f45676f3bb190c
Njeri <njericooper@gmail.com>
2019-07-24 17:22:55 -0500

    regex practice

---
+++ regexPractice.js
@@ -0,0 +1,11 @@
+const a = "The quick brown fox jumps over the lazy dog.";
+const b = "There were 614 instances of students getting 90.0% or above.";
+const c = "The FCC had to censor the network for saying &$#*@!.";
+
+let match = /./.test(a);
+let otherMatch = /./.test(b);
+let theOtherMatch = /./.test(c);
+
+console.log(match);
+console.log(otherMatch);
+console.log(theOtherMatch);
