368945dcac3ba0d09a6b00539d23c75ab4f34935
Njeri <njericooper@gmail.com>
2019-08-14 19:42:47 -0500

    Making a food log. Adding demo files

---
+++ food-log/scripts/firebase.js
@@ -0,0 +1,13 @@
+/*const config = {
+  apiKey: "AIzaSyCR_gdWO1dtRqPLE5Vo7RNGoPi0ceG0dRs",
+  authDomain: "health-tracker-ce950.firebaseapp.com",
+  databaseURL: "https://health-tracker-ce950.firebaseio.com",
+  projectId: "health-tracker-ce950",
+  storageBucket: "",
+  messagingSenderId: "259382991432"
+};
+firebase.initializeApp(config);
+
+export const provider = new firebase.auth.GoogleAuthProvider();
+export const auth = firebase.auth();
+export default firebase;*/
