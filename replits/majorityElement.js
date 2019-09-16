abcfd6353b7e5acf875fe3f48db65b60bc12b8ef
Njeri <njericooper@gmail.com>
2019-09-16 11:08:15 -0500

    another answer

--- replits/majorityElement.js
+++ replits/majorityElement.js
@@ -1,4 +1,4 @@
-var majorityElement = function(nums) {
+/*var majorityElement = function(nums) {
     var arr = [3, 4, 3, 5, 4, 3];
     var majorityCount = 1;
     var arrayCountOfMajority = 0;
@@ -15,18 +15,42 @@ var majorityElement = function(nums) {
             }
         }
         arrayCountOfMajority = 0;
+        return theNumberThatOccursMost;
     }
-console.log(theNumberThatOccursMost+ " occurs " +majorityCount +" times ");
+    
 };
 
-majorityElement();
+//majorityElement();
+console.log(majorityElement(theNumberThatOccursMost));*/
 
-/* initialize num and a counter  i with 0, 
+/* initialize currentArrayNumber and a counter  i with 0, 
 for each element x in the input:
-- if i = 0 then assign num = x and i = 1, 
-- else if num = xthen ssign i = i+1
+- if i = 0 then assign currentArrayNumber = x and i = 1, 
+- else if currentArrayNumber = xthen ssign i = i+1
 - else assign i = i-1
 
-return num
+return currentArrayNumber
 
 */
+
+var majorityElement = function(nums) {
+    var majorityCount = (nums.length / 2 | 0);
+            for (var i = 0; i < nums.length; i++) {
+                var currentArrayNumber = nums[i];
+                {
+                    var count = 0;
+                    for (var j = 0; j < nums.length; j++) {
+                        var elem = nums[j];
+                        {
+                            if (elem === currentArrayNumber) {
+                                count += 1;
+                            }
+                        }
+                    }
+                    if (count > majorityCount) {
+                        return currentArrayNumber;
+                    }
+                }
+            }
+            return -1;
+    };
