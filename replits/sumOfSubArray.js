568f9d4eada8f4ba096a894e735c2a76c4ce919f
Njeri <njericooper@gmail.com>
2019-09-18 11:35:34 -0500

    sum of sub array equals k

---
+++ replits/sumOfSubArray.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+subarraySum = function(nums, k) {
+    let subArrLen = 0, 
+        subArrSum = 0,
+        history = {0: -1};
+        for(i=0; i< nums.length;i++){
+            subArrSum += nums[1];
+            if(history.hasOwnProperty(subArrSum-k))
+        }
+    return subArrSum;
+}
+
+/* subarraySum = function(nums, k) {
+    let subArrLen = 0, 
+        subArrSum = 0,
+        history = {0: -1};
+        for(i=0; i< nums.length;i++){
+            subArrSum += nums[1];
+        }
+    return subArrSum;
+} */
