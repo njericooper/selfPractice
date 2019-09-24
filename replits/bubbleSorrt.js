f71279e8a47fa70188b801cca68f811919ae3df8
Njeri <njericooper@gmail.com>
2019-09-24 13:46:17 -0500

    bubble sort

---
+++ replits/bubbleSorrt.js
@@ -0,0 +1,16 @@
+var bubbleSort = nums => {  
+    do {
+        var swapped = false;
+        for (var i = 0; i < nums.length; i++) {
+            if (nums[i] > nums[i+1]) {
+                var temp = nums[i];
+                nums[i] = nums[i+1];
+                nums[i+1] = temp;
+                swapped = true;
+            }
+        }
+    } while(swapped);
+    return nums;
+};
+
+console.log(bubbleSort[5, 4, 7, 2]);
