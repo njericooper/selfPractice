5a7e30ade9fbdb68cd63110af170b316a02e2386
njericooper@gmail.com <njericooper@gmail.com>
2019-11-15 21:38:55 -0600

    update

--- FEM-React/adopt-me/src/App.js
+++ FEM-React/adopt-me/src/App.js
@@ -1,18 +1,33 @@
-const Pet = () => {
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Pet = ({ name, animal, breed }) => {
     return React.createElement("div", {}, [
-      React.createElement("h1", {}, "Luna"),
-      React.createElement("h2", {}, "Dog"),
-      React.createElement("h2", {}, "Havanese")
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed)
     ]);
-  };
-  
-  const App = () => {
-    return React.createElement("div", {}, [
-      React.createElement("h1", {}, "Adopt Me!"),
-      React.createElement(Pet),
-      React.createElement(Pet),
-      React.createElement(Pet)
+};
+
+const App = () => {
+    return React.createElement("div", { id: "something-important" }, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+        name: "Wolfgang",
+        animal: "dog",
+        breed: "Akita"
+    }),
+    React.createElement(Pet, {
+        name: "Ty",
+        animal: "dog",
+        breed: "Boston Terrier"
+    }),
+    React.createElement(Pet, {
+        name: "Fletch",
+        animal: "dog",
+        breed: "GP"
+    })
     ]);
-  };
-  
-  ReactDOM.render(React.createElement(App), document.getElementById("root"));
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
