20fd840fd7731c4b6a3c4fe890af8e25ff6f923a
njericooper@gmail.com <njericooper@gmail.com>
2019-11-15 22:15:15 -0600

    updates

--- FEM-React/adopt-me/src/App.js
+++ FEM-React/adopt-me/src/App.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const Pet = ({ name, animal, breed }) => {
-    return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed)
-    ]);
-};
+import Pet from './Pet';
 
 const App = () => {
     return React.createElement("div", { id: "something-important" }, [
@@ -25,7 +18,7 @@ const App = () => {
     React.createElement(Pet, {
         name: "Fletch",
         animal: "dog",
-        breed: "GP"
+        breed: "Great P"
     })
     ]);
 };
