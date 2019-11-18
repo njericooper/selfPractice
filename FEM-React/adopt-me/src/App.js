c4bd04b20824dd7a9883c2f0feeab6a6ba3cdee3
njericooper@gmail.com <njericooper@gmail.com>
2019-11-17 20:29:34 -0600

    Stateful logic with hooks

--- FEM-React/adopt-me/src/App.js
+++ FEM-React/adopt-me/src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Pet from './Pet';
+//import Pet from './Pet';
+import SearchParams from './SearchParams' 
 
 const App = () => {
-    return React.createElement("div", { id: "something-important" }, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-        name: "Wolfgang",
-        animal: "dog",
-        breed: "Akita"
-    }),
-    React.createElement(Pet, {
-        name: "Ty",
-        animal: "dog",
-        breed: "Boston Terrier"
-    }),
-    React.createElement(Pet, {
-        name: "Fletch",
-        animal: "dog",
-        breed: "Great P"
-    })
-    ]);
+
+
+return(
+    <div>
+        <h1 id="something-important"></h1>
+        {/*<Pet name="Wolfgang" animal="Dog" breed="Akita"/>
+        <Pet name="Ty" animal="Dog" breed="Boston T"/>
+<Pet name="Fletch" animal="Dog" breed="Great Per"/> */}
+        <SearchParams />
+    </div>
+)
+
 };
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
