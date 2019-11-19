966a386ba2c3538f07b0826cb9bd2be298b5d152
njericooper@gmail.com <njericooper@gmail.com>
2019-11-19 13:50:16 -0600

    added dropdown with optopns and option keys

--- FEM-React/adopt-me/src/SearchParams.js
+++ FEM-React/adopt-me/src/SearchParams.js
@@ -1,14 +1,34 @@
 import React, { useState } from 'react';
+import { ANIMALS } from '@frontendmasters/pet';
 
 const SearchParams = () => {
     const [location, setLocation] = useState("Seattle, WA");
+    const [animal, setAnimal] = useState("dog");
     return( 
         <div className="search-params">
             <form >
                 <label htmlFor="location">
                     Location
-                    <input id ="location" value={location}
-                    placeholder="Location" onChange={e => setLocation (e.target.value)} />
+                    <input id ="location" 
+                    value={location}
+                    placeholder="Location" 
+                    onChange={e => setLocation (e.target.value)
+                    } />
+                </label>
+                <label htmlFor="animal">
+                    Animal
+                    <select
+                    id="animal"
+                    value={animal}
+                    onChange= {e => setAnimal(e.target.value)}
+                    onBlur={e => setAnimal(e.target.value)}>
+                        <option>All</option>
+                        {ANIMALS.map(animal => (
+                            <option key={animal} value={animal}>
+                                {animal}
+                            </option>
+                        ))}
+                    </select>
                 </label>
                 <button>Submit</button>
             </form>
